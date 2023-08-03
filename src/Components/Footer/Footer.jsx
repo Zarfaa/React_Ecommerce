94254f7325cd47821f483419c12f1659dc41a4d0
Zarfaa <zarfamasood924@gmail.com>
2023-08-03 21:31:30 +0500

    Changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,21 @@
-
+import {Link} from "react-router-dom"
 import "./footer.css"
 
 let Footer = () =>{
     return(
       <footer className="Footer__container">
-
     <div>
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/AboutUS">About</Link></li>
+        <li><Link to="/ContactUS">Contact</Link></li>
       </ul>
     </nav>
     </div>
-
     <div>
     <p>&copy; 2023 Fashion, Inc. All rights reserved.</p>
     </div>
-
     </footer>
   )
 }
