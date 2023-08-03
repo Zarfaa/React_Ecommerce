504a68d3b2a2ba89b7cc1115080abd76212df463
Zarfaa <zarfamasood924@gmail.com>
2023-08-03 14:19:49 +0500

    Changes

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,59 +1,56 @@
 import "./NavBar.css"
 import React from "react"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux'
 
 let NavBar = () => {
   const state = useSelector(state => state.handleCart)
- return(
-    <nav>
-  <div className="nav_container">
+  return (
+    <header>
+      <div className="nav_container">
 
-  <Link  to="/" >
-  <div className="nav_logo">
-  Fashion
-  </div>
-  </Link>
+        <Link to="/" >
+          <div className="nav_logo">
+            Fashion
+          </div>
+        </Link>
 
-  <div className="nav_items">
+        <div className="nav_items">
 
-  <Link  to="/"> 
-   <div>Home </div>
-  </Link>
+          <Link to="/">
+            <div>Home </div>
+          </Link>
 
-  <Link  to="/AboutUS">
-  <div>
-  About Us
-  </div>
-  </Link>
+          <Link to="/AboutUS">
+            <div>
+              About Us
+            </div>
+          </Link>
 
-  <Link  to="/ContactUS">
-  <div>
-  Contact Us
-  </div>
-  </Link>
+          <Link to="/ContactUS">
+            <div>
+              Contact Us
+            </div>
+          </Link>
 
-  </div>
+        </div>
 
 
-  <div className="nav_btn">
+        <div className="nav_btn">
+          <Link to="/signup">
+            <div >
+              <i class="uil uil-user"></i>SignUP
+            </div>
+          </Link>
+          <Link to="/cart" >
+            <div >
+              <i class="uil uil-shopping-bag"></i>Cart ({state.length})
+            </div>
+          </Link>
+        </div>
 
-  <Link  className="signup_btn" to="/signup">
-  <div >
-  <i class="uil uil-user"></i>SignUP
-  </div>
-  </Link>
-
-  <Link className="cart_btn"  to="/cart" >
-  <div >
-  <i class="uil uil-shopping-bag"></i>Cart ({state.length}) 
-  </div>
-  </Link>
-  
-  </div>
-
-  </div>
-</nav>
- )
+      </div>
+    </header>
+  )
 }
 export default NavBar
